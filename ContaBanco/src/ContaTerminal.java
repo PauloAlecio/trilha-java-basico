29fe33707eca6ec6e1f80eab29ccbad2cf972c24
Paulo Alecio <paulo.alecio@mobicare.com.br>
2023-04-28 23:00:31 -0300

    Desafio concluído

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+     static int numero = 0;
+     static String agencia ="";
+     static String nomeCliente ="";
+     static float saldo =0.0f;
+
+    static Scanner sc = new Scanner(System.in);
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println("Por favor digite número da Agência !");
+        numero = sc.nextInt();
+
+        System.out.println("Agora informe a Agência !");
+        agencia = sc.next();
+       
+        sc.nextLine();
+
+        System.out.println("Agora informe Nome Cliente !");
+        nomeCliente = sc.nextLine();
+
+        sc.nextLine();
+
+        System.out.println("Por último informe seu Saldo !");
+        saldo = sc.nextFloat();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+         nomeCliente, agencia, numero, saldo);
+        
+
     }
 }
