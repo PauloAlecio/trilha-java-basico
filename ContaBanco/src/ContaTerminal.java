d0e51a151f4144e7c11f8a57728bcd63cea4abfe
PauloAlecio <pauloalecioreuel@gmail.com>
2023-04-29 01:23:00 -0300

    Atualizando solução

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-     static int numero = 0;
-     static String agencia ="";
-     static String nomeCliente ="";
-     static float saldo =0.0f;
-
-    static Scanner sc = new Scanner(System.in);
-
-
     public static void main(String[] args) throws Exception {
-        
+        int numero = 0;
+        String agencia ="";
+        String nomeCliente ="";
+        float saldo =0.0f;
+   
+       try (Scanner sc = new Scanner(System.in)) {
         System.out.println("Por favor digite número da Agência !");
         numero = sc.nextInt();
 
@@ -27,7 +24,7 @@ public class ContaTerminal {
 
         System.out.println("Por último informe seu Saldo !");
         saldo = sc.nextFloat();
-
+    }
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
          nomeCliente, agencia, numero, saldo);
         
